a52794538880fc194cc20e333d7076e7bd9b5f60
devillers <davidevillers@gmail.com>
2022-06-22 23:20:52 +0200

    accueil

--- pages/product/index.js
+++ pages/product/index.js
@@ -1,9 +1,8 @@
 import Layout from '../../components/Layout';
 import ProductItem from '../../components/ProductItem';
-import Product from '../../models/Product';
-import db from '../../utils/db';
+import data from '../../utils/data';
 
-const Home = ({ products }) => {
+export default function Home() {
   return (
     <Layout title="location linge">
       <div className="rounded-md shadow-md text-xs text-slate-900 font-poppin p-5">
@@ -11,8 +10,8 @@ const Home = ({ products }) => {
         <div className="grid grid-cols-5 ">
           <div className="col-span-3">
             <div className="grid grid-cols-3 gap-3">
-              {products.map((product) => (
-                <ProductItem product={product} key={product._id} />
+              {data.products.map((product) => (
+                <ProductItem product={product} key={product.slug} />
               ))}
             </div>
           </div>
@@ -20,24 +19,4 @@ const Home = ({ products }) => {
       </div>
     </Layout>
   );
-};
-
-export async function getStaticProps() {
-  await db.connect();
-
-  const res = await Product.find({});
-  const products = res.map((doc) => {
-    return doc.toObject({
-      tranform: (doc, ret) => {
-        ret._id = doc._id.toString();
-      },
-    });
-  });
-  return {
-    props: {
-      products,
-    },
-  };
 }
-
-export default Home;
